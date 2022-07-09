1dc3f8a34b4d6ef296c07e91019c3556a50834d2
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-09 16:11:19 +0530

    Added :Welcome message

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -2,6 +2,7 @@ package com.bridgelabz;
 
 public class LineComparison {
     public static void main(String[] args) {
+
         System.out.println("Welcome to Line Comparison Computation");
     }
 }
